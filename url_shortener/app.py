3f250f480877834acf68488116cc4889852fe3ae
nikart42 <nikart42@yandex-team.ru>
2017-10-22 23:41:50 +0200

    tornado version

--- url_shortener/app.py
+++ url_shortener/app.py
@@ -1,75 +1,86 @@
 # -*- coding: utf-8 -*-
 
-from datetime import datetime, timedelta
-import sys
+from datetime import datetime
 from urllib.parse import urlparse
+import json
 
-from flask import Flask, request, redirect, jsonify, make_response
-from werkzeug.exceptions import BadRequest, NotFound
-from flask_pymongo import PyMongo
+
+from tornado.ioloop import IOLoop
+import tornado.web
+from tornado.httpserver import HTTPServer
+from motor import MotorClient
 from pymongo.errors import DuplicateKeyError
 import validators
 
 from url_shortener import base62
 
-app = Flask('url_shortener')
-app.config['MONGO_URI'] = 'mongodb://localhost:27017'
-mongo = PyMongo(app)
+app = tornado.web.Application(debud=True)
+
+server = HTTPServer(app)
+server.bind(80)
+server.start(8)
 
 
-@app.before_first_request
-def ensure_indexes():
-    mongo.db.urls.create_index('last_access',
-                               expireAfterSeconds=timedelta(days=365).total_seconds())
-    mongo.db.urls.create_index('url')
+class ResolveURLHandler(tornado.web.RequestHandler):
+    def __init__(self, application, request, **kwargs):
+        self.urls_coll = application.settings['db'].url_shortener.urls
+        super().__init__(application, request, **kwargs)
 
-
-@app.route('/shorten_url', methods=['POST'])
-def shorten_url():
-    json_data = request.json
-    if json_data is None:
-        raise BadRequest
-
-    url = json_data.get('url')
-    if not url:
-        raise BadRequest('Missing url to shorten')
-    else:
-        if not urlparse(url).scheme:
-            url = 'http://' + url
-        if not validators.url(url):
-            raise BadRequest('Malformed url')
-
-        existing_record = mongo.db.urls.find_one({'url': url})
-        if existing_record:
-            return make_response(jsonify({'shortened_url': request.host_url + existing_record['_id']}), 200)
-        else:
-            new_record = {'_id': base62.generate_id(),
-                          'last_access': datetime.utcnow(),
-                          'url': url}
-            inserted = False
-            while not inserted:
-                try:
-                    mongo.db.urls.insert_one(new_record)
-                    inserted = True
-                except DuplicateKeyError:
-                    new_record['_id'] = base62.generate_id()
-            return make_response(jsonify({'shortened_url': request.host_url + new_record['_id']}), 201)
-
-
-@app.route('/<short_id>', methods=['GET'])
-def process_shortened_request(short_id):
-    if base62.validate(short_id):
-        url_record = mongo.db.urls.find_one({'_id': short_id})
+    async def get(self, short_id):
+        url_record = await self.urls_coll.find_one_and_update({'_id': short_id},
+                                                              {'$set': {'last_access': datetime.utcnow()}})
         if url_record:
-            mongo.db.urls.update_one({'_id': short_id},
-                                     {'$set': {'last_access': datetime.utcnow()}})
-            return redirect(url_record['url'])
+            self.redirect(url_record['url'])
         else:
-            raise NotFound('Shortened url not found')
-    else:
-        raise BadRequest('Malformed shortened url')
+            self.send_error(404, reason='Shortened url not found')
 
 
-@app.route('/')
-def index():
-    return 'Hello, World!'
+class ShortenURLHandler(tornado.web.RequestHandler):
+    def __init__(self, application, request, **kwargs):
+        self.urls_coll = application.settings['db'].url_shortener.urls
+        super().__init__(application, request, **kwargs)
+
+    def prepare(self):
+        if self.request.body:
+            try:
+                self.request.json = json.loads(self.request.body)
+            except ValueError:
+                self.send_error(400, reason='Malformed request')  # Bad Request
+
+    async def post(self):
+        url = self.request.json.get('url')
+        if not url:
+            self.send_error(400, reason='Missing url to shorten')
+            return
+        else:
+            if not urlparse(url).scheme:
+                url = 'http://' + url
+            if not validators.url(url):
+                self.send_error(400, reason='Malformed url')
+                return
+
+            existing_record = await self.urls_coll.find_one({'url': url})
+            if existing_record:
+                self.set_status(200)
+                self.write({'shortened_url': self.request.host + existing_record['_id']})
+                self.finish()
+            else:
+                new_record = {'_id': base62.generate_id(),
+                              'last_access': datetime.utcnow(),
+                              'url': url}
+                inserted = False
+                while not inserted:
+                    try:
+                        await self.urls_coll.insert_one(new_record)
+                        inserted = True
+                    except DuplicateKeyError:
+                        new_record['_id'] = base62.generate_id()
+                self.set_status(201)
+                self.write({'shortened_url': self.request.host + new_record['_id']})
+                self.finish()
+
+
+app.add_handlers(".*$", [(r'/shorten_url', ShortenURLHandler),
+                         (r'/(\w+)', ResolveURLHandler)])
+app.settings['db'] = MotorClient('mongodb://localhost:27017')
+IOLoop.instance().start()
